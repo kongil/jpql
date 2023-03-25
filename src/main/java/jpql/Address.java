301cf1a8f8931c87c59c86d6669598f794a2850f
kongil <o_o0122@naver.com>
2023-03-25 17:15:47 +0900

    Initial commit JPQL 문법  - 엔티티와 속성은 대소문자 구분  - JPQL 키워드는 대소문자 구분X  - 엔티티 이름 사용, 테이블 이름x  - 별칭은 필수(m)

---
+++ src/main/java/jpql/Address.java
@@ -0,0 +1,34 @@
+package jpql;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String city;
+    private String stree;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStree() {
+        return stree;
+    }
+
+    public void setStree(String stree) {
+        this.stree = stree;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em =  emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username", "member1");
+            Member singleResult = query.getSingleResult();
+            System.out.println("singleResult = " + singleResult.getUsername());
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2 " + (m1 instanceof Member));
+        System.out.println("m1 == m2 " + (m2 instanceof Member));
+    }
+}
