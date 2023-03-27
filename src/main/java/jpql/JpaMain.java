8b9095699575b6c5f987b1bbdd5766215e3dab08
kongil <o_o0122@naver.com>
2023-03-27 23:22:19 +0900

    프로젝션  프로젝션 대상 : 엔티티, 임베디드 타입, 스칼라 타입(기본 타입)  여러 값 조회 시.    1. Query 타입으로 조회    2. Object[] 타입으로 조회    3. new 명령어로 조회       -> 패키지 명을 포함한 전체 클래스명 입력       -> 순서와 타입이 일치하는 생성자 필요

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,10 +19,15 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query.setParameter("username", "member1");
-            Member singleResult = query.getSingleResult();
-            System.out.println("singleResult = " + singleResult.getUsername());
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println("username = " + memberDTO.getUsername());
+            System.out.println("age = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
