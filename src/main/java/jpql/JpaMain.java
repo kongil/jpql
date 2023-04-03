2cbd0acf98f248d2eee438d14c9957eacc5e0e98
kongil <o_o0122@naver.com>
2023-04-03 23:48:33 +0900

    경로 표현식  - 상태 필드 : 경로 탐색의 끝, 탐색(x) - 추가로 .을 통해 속성을 찾는것  - 단일 값 연관 경로 : 묵시적 내부 조인 발생, 탐색 o   권장 x(묵시적 조인 튜닝 어렵)  - 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색 x   권장 x     - FROM 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가,

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -20,24 +21,23 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자1");
+            member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) From Member m ";
+            String query = "select t.members from Team t ";
 
-            List<String> result = em.createQuery(query, String.class)
+            Collection result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
