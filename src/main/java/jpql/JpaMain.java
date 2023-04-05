dfa1d57f50b6497c2755fc7ee4604f4c643d6544
kongil <o_o0122@naver.com>
2023-04-05 23:56:31 +0900

    페치 조인(fetch join)  - JPQL 성능 최적화  - 연관 엔티티 컬렉션 LAZY BINDING 없이 한번에 함꼐 조회 가능하게 하는 기능  - N : 1 잘 동작, but 1 : N DISTINCT을 붙여 엔티티 중복 제거해야함

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,29 +14,43 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select t.members from Team t ";
+            String query = "select t from Team t join fetch t.members";
 
-            Collection result = em.createQuery(query, Collection.class)
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            for (Team team : result) {
+                System.out.println("team.getName() = " + team.getName() + "|" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
+            }
 
             tx.commit();
         } catch (Exception e) {
